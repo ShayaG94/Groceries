ad65b37994feb3136db73e9a3d34d7f0dbf0e9ee
Shaya94 <yginzburg94@gmail.com>
2022-12-26 21:45:36 +0200

    Add virtuals to db models, apply in views

--- models/product.js
+++ models/product.js
@@ -16,8 +16,29 @@ const PurchaseSchema = new Schema({
     },
 });
 
-PurchaseSchema.virtual("usagePeriodLength").get(function () {
+PurchaseSchema.virtual("usageDates").get(function () {
     return `${this.usagePeriod.startDate} - ${this.usagePeriod.endDate}`;
+});
+
+const splitDate = (date) => {
+    return { day, month, year };
+};
+
+const UTCizeDate = (date) => {
+    const [day, month, year] = date.split("/");
+    rawDate = new Date(year, month - 1, day);
+    return Date.UTC(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate());
+};
+
+const calcDaysDifference = (startDate, endDate) => {
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    return Math.floor((endDate - startDate) / _MS_PER_DAY);
+};
+
+PurchaseSchema.virtual("daysUsed").get(function () {
+    const startDate = UTCizeDate(this.usagePeriod.startDate);
+    const endDate = UTCizeDate(this.usagePeriod.endDate);
+    return calcDaysDifference(startDate, endDate) + 1;
 });
 
 const ProductSchema = new Schema({
