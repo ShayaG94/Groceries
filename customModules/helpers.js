25202406c13e0e69ee96a77e834d57933c5330f4
Shaya94 <yginzburg94@gmail.com>
2023-01-08 20:14:00 +0200

    Remake helper funcs again

--- customModules/helpers.js
+++ customModules/helpers.js
@@ -1,19 +1,18 @@
-const UTCizeDate = function (date) {
+function UTCizeDate(date) {
     const [day, month, year] = date.split("/");
     rawDate = new Date(year, month - 1, day);
     return Date.UTC(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate());
-};
-
-const calcDaysDifference = function (startDate, endDate) {
+}
+function calcDaysDifference(startDate, endDate) {
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
     return Math.floor((endDate - startDate) / _MS_PER_DAY);
-};
+}
 
-const stringifyDate = function (date) {
+function stringifyDate(date) {
     if (date) {
         const [year, month, day] = date.split("-");
         return `${day}/${month}/${year}`;
     } else return "";
-};
+}
 
 module.exports = { UTCizeDate, calcDaysDifference, stringifyDate };
