b5b6a1b7c792de643fda833922cde5cfb8bc22c0
Shaya94 <yginzburg94@gmail.com>
2023-01-08 20:11:52 +0200

    Remake helper functions from arrow funcs to regular

--- customModules/helpers.js
+++ customModules/helpers.js
@@ -1,15 +1,15 @@
-const UTCizeDate = (date) => {
+const UTCizeDate = function (date) {
     const [day, month, year] = date.split("/");
     rawDate = new Date(year, month - 1, day);
     return Date.UTC(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate());
 };
 
-const calcDaysDifference = (startDate, endDate) => {
+const calcDaysDifference = function (startDate, endDate) {
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
     return Math.floor((endDate - startDate) / _MS_PER_DAY);
 };
 
-const stringifyDate = (date) => {
+const stringifyDate = function (date) {
     if (date) {
         const [year, month, day] = date.split("-");
         return `${day}/${month}/${year}`;
