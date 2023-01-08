1e0cffdea54f2453573c1ade720eefbf9c1af565
Shaya94 <yginzburg94@gmail.com>
2023-01-09 01:09:15 +0200

    Update Products List page styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,23 @@
 const productCards = document.querySelectorAll(".productIndexCard");
+const categoriesLinks = document.querySelectorAll(".productIndexCard a");
 
 productCards.forEach((card) => {
-    card.addEventListener("mouseover", (e) => (e.target.style.cursor = "pointer"));
+    card.addEventListener("mouseover", function (e) {
+        e.target.style.cursor = "pointer";
+        this.childNodes[1].childNodes[1].style.textDecoration = "underline";
+    });
+    card.addEventListener("mouseout", function (e) {
+        this.childNodes[1].childNodes[1].style.textDecoration = "";
+    });
     card.addEventListener("click", (e) => {
         const productPath = card.getAttribute("data-path");
         window.location = `products/${productPath}`;
+    });
+});
+
+categoriesLinks.forEach((link) => {
+    link.addEventListener("mouseover", function (e) {
+        this.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].style.textDecoration = "";
+        e.stopPropagation();
     });
 });
