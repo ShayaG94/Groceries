20ecdf2863a20ef9a9e578dcaac87aca5831e80a
Shaya94 <yginzburg94@gmail.com>
2023-01-13 20:23:26 +0200

    Update css file with root vars

--- public/js/script.js
+++ public/js/script.js
@@ -4,10 +4,10 @@ const categoriesLinks = document.querySelectorAll(".productIndexCard a");
 productCards.forEach((card) => {
     card.addEventListener("mouseover", function (e) {
         e.target.style.cursor = "pointer";
-        this.childNodes[1].childNodes[1].style.textDecoration = "underline";
+        this.childNodes[1].style.textDecoration = "underline";
     });
     card.addEventListener("mouseout", function (e) {
-        this.childNodes[1].childNodes[1].style.textDecoration = "";
+        this.childNodes[1].style.textDecoration = "";
     });
     card.addEventListener("click", (e) => {
         const productPath = card.getAttribute("data-path");
@@ -17,17 +17,13 @@ productCards.forEach((card) => {
 
 categoriesLinks.forEach((link) => {
     link.addEventListener("mouseover", function (e) {
-        this.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].style.textDecoration = "";
+        this.parentNode.parentNode.childNodes[1].style.textDecoration = "";
         e.stopPropagation();
     });
 });
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
-
-const myModal = document.querySelector(".modal");
-const modal = bootstrap.Modal.getInstance(myModal);
-const modalSubmit = document.querySelector("#categoryModal > div > div > div.modal-footer.border.border-0.pt-0 > button.btn.btn-info");
 
 function addCat() {
     const newCat = document.querySelector("#categoryNew").value;
