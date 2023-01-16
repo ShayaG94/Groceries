92526657d7d63664c797bf90de2d2680a88db9fe
Shaya94 <yginzburg94@gmail.com>
2023-01-16 18:02:08 +0200

    Update product tabs functionality. Add btn-secondary styling

--- public/js/productScript.js
+++ public/js/productScript.js
@@ -1,20 +1,19 @@
-const queriesIndex = window.location.href.indexOf("?");
-const url = window.location.href.slice(0, queriesIndex);
-
 const tabNameIndex = window.location.href.indexOf("=") + 1;
 const tabName = window.location.href.slice(tabNameIndex);
+const tabButton = document.querySelector(`#${tabName}`);
+const tabPane = document.querySelector(`#${tabName}-tab-pane`);
+tabButton.classList.add("active");
+tabPane.classList.add("show");
+tabPane.classList.add("active");
 
-if (queriesIndex > 0) {
-    window.addEventListener("load", () => {
-        window.history.replaceState("", "", url);
-    });
-    const tabButton = document.querySelector(`#${tabName}-tab`);
-    const tabPane = document.querySelector(`#${tabName}-tab-pane`);
-    tabButton.classList.add("active");
-    tabPane.classList.add("show");
-    tabPane.classList.add("active");
-} else {
-    document.querySelector("#stats-tab").classList.add("active");
-    document.querySelector("#stats-tab-pane").classList.add("active");
-    document.querySelector("#stats-tab-pane").classList.add("show");
-}
+const tabs = document.querySelectorAll("button.nav-link");
+tabs.forEach((tab) =>
+    tab.addEventListener("click", (e) => {
+        const tabName = e.target.id;
+        const tabNameIndex = window.location.href.indexOf("=") + 1;
+        if (tabNameIndex > 0) {
+            const url = window.location.href.slice(0, tabNameIndex);
+            window.history.replaceState("", "", url + tabName);
+        }
+    })
+);
